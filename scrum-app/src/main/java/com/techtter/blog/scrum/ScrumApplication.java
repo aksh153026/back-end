cbd099a0d2b3280224c1abc6ff91f7794ac087ec
aksh153026 <57639109+aksh153026@users.noreply.github.com>
2021-06-25 22:25:16 +0530

    Update ScrumApplication.java

--- scrum-app/src/main/java/com/techtter/blog/scrum/ScrumApplication.java
+++ scrum-app/src/main/java/com/techtter/blog/scrum/ScrumApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//hi akshaya
+//hi akshaya singh
 @SpringBootApplication
 @EntityScan("com.techtter.blog.scrum.model")
 @EnableJpaRepositories("com.techtter.blog.scrum.repository")
